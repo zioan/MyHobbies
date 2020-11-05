60b84e3381bb4f25d801fc18c23ca81ba44fed57
Zaharia Ioan <zaharia.ioan@gmail.com>
2020-11-05 19:51:09 +0100

    DB connection

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Hello Laravel
+                   Hello {{ auth()->user()->name }}
                 </div>
 
                 <div class="links">
